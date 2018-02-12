d135a630031f97086db9ebf7d2c7852c92029714
Lennaft <lenny1842002@gmail.com>
2018-02-12 23:20:01 +0100

    Update index.php

--- q/height/index.php
+++ q/height/index.php
@@ -2,7 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://node.ultranote.org:31000/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://loncoin.net:32112/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 curl_close($ch);
