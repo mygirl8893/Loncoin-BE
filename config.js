f7892822e18ce480460c7c215786b5ada4cfae3a
Lennaft <lenny1842002@gmail.com>
2018-02-13 00:37:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var api = 'http://loncoin.net:32112';
 var blockTargetInterval = 140;
 var coinUnits = 1000000;
-var symbol = 'LCN';
+var symbol = 'XUN';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
